5fbe88c90de2b1b21853246dc2c3fd5fbda0c62b
Mayconq96 <mscrat@hotmail.com>
2015-08-29 22:07:26 -0300

    Organização Inicial

--- Java-code/src/sigma/SIGMA.java
+++ Java-code/src/sigma/SIGMA.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sigma;
-
+import java.util.Scanner;
 /**
  * @author Anderson Bottega
  * @author Juliano Felipe
  * @author Maycon Queiroz
  */
 public class SIGMA {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
