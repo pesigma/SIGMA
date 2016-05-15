4bbe381f14c977d7e2673399e69a787689b36407
Maycon <mscrat@hotmail.com>
2016-05-15 18:08:51 -0300

    Atualização Instalador

--- Instalador-SIGMA.bat
+++ Instalador-SIGMA.bat
@@ -6,12 +6,12 @@ echo ------------------------------
 md C:\Users\%username%\Documents\SIGMA
 md C:\Users\%username%\Documents\SIGMA\Relatorios
 md C:\Users\%username%\Documents\SIGMA\App
-copy Java-code\*.* C:\Users\%username%\Documents\SIGMA\App
-copy sigma-icon.ico C:\Users\%username%\Documents\SIGMA
-copy Manual.pdf C:\Users\%username%\Documents\SIGMA
-copy NR.exe C:\Users\%username%\Documents\SIGMA
-copy ShortcutCreator.bat C:\Users\%username%\Documents\SIGMA
+xcopy Java-code\*.* C:\Users\%username%\Documents\SIGMA\App /s
+xcopy sigma-icon.ico C:\Users\%username%\Documents\SIGMA
+xcopy Manual.pdf C:\Users\%username%\Documents\SIGMA
+xcopy NR.exe C:\Users\%username%\Documents\SIGMA
+xcopy ShortcutCreator.bat C:\Users\%username%\Documents\SIGMA
 C:
 cd C:\Users\%username%\Documents\SIGMA
 ShortcutCreator.bat
-del C:\Users\%username%\Documents\SIGMA\ShortcutCreator.bat
+del /q C:\Users\%username%\Documents\SIGMA\ShortcutCreator.bat
