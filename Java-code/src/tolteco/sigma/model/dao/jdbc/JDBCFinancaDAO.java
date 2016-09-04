0b6b43f1541e858b80081d1c548bb975ff75ccda
Maycon <mscrat@hotmail.com>
2016-09-04 20:57:17 -0300

    Alterações do escritor de relatórios
    * Tamanho das fontes vai ter que cair; o programa tá ficando com uma
    cara estranha
    * Data nas tabelas vai ter que juntar de novo
    * Chave primária do banco de dados deve ser composta pela chave
    estrangeira (MajorVersion) e chave local (MinorVersion) do contrário
    versões com minor version menores não vão poder sair
    * Prioridade: Continuar a modularização
    * Desenvolver os DAO e JDBC para operação perfeita ("Modularizada, com
    querys, etc.")
    * Documentar funções e códigos não documentados
    * Definir em plebicito se usa List ou ArrayList
    * A partir de agora todas as submissões no GitHub devem ter uma cópia no
    banco (como esta já tem (ou pelo o menos deveria de acordo com o que eu
    fiz))
    * Log da tela inicial deve ser adequadamente configurado
    
    Função de busca de registros pré implementada aguardando a implementação
    de métodos do banco ou ajustes extras. Inserido um exemplo de Latex
    junto na pasta e seu respectivo resultado.

--- Java-code/src/tolteco/sigma/model/dao/jdbc/JDBCFinancaDAO.java
+++ Java-code/src/tolteco/sigma/model/dao/jdbc/JDBCFinancaDAO.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tolteco.sigma.model.dao.jdbc;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import tolteco.sigma.model.dao.DatabaseException;
 import tolteco.sigma.model.dao.FinancaDAO;
@@ -14,7 +11,7 @@ import tolteco.sigma.model.entidades.Financa;
  *
  * @author Juliano_Felipe
  */
-public class JDBCFinancaDAO extends JDBCAbstractDAO implements FinancaDAO{
+public class JDBCFinancaDAO extends JDBCAbstractDAO implements FinancaDAO {
 
     @Override
     public boolean insert(Financa t) throws DatabaseException {
@@ -45,5 +42,32 @@ public class JDBCFinancaDAO extends JDBCAbstractDAO implements FinancaDAO{
     public List<Financa> select(String nome) throws DatabaseException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    /**
+     * 04/09 - Maycon Funcao que retorna um Array de registros para serem
+     * insridos no Latex
+     *
+     * @param tipo
+     * @return
+     * @throws DatabaseException
+     */
+    public List<Financa> toReport(int tipo) throws DatabaseException {
+        String data = new SimpleDateFormat("YYYY-MM-dd").format(new Date());
+        if (tipo == 0) { //Mensal
+            int mes = Integer.parseInt(data.substring(5, 7)) - 1;
+            if (mes == 0) {
+                mes = 12;
+            }
+            String data2 = data.substring(0, 4) + "-" +  mes + "-" + data.substring(8, 10);
+            String sql = "SELECT * FROM Financa WHERE date BETWEEN \"" + data + "\" AND \"" + data2 + "\""; //Erro de agrupamento de data
+            //Falta o resto... esperando codigo
+        } else if (tipo == 1) { //Anual
+            int ano = Integer.parseInt(data.substring(0, 4)) - 1;
+            String data2 = ano + "-" + data.substring(5, 7) + "-" + data.substring(8, 10);
+            String sql = "SELECT * FROM Financa WHERE date BETWEEN \"" + data + "\" AND \"" + data2 + "\""; //Erro de agrupamento de data
+            //Falta o resto... esperando codigo
+        }
+        return null;
+    }
+
 }
--- Java-code/src/tolteco/sigma/model/entidades/FinancaTipo.java
+++ Java-code/src/tolteco/sigma/model/entidades/FinancaTipo.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tolteco.sigma.model.entidades;
 
 /**
- *
+ * Identifica o tipo de uma financa cadastrada no sistema
  * @author Juliano Felipe
  */
 public enum FinancaTipo {
